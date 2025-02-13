7da1966d08b0f0e3151d189f59fda4dcc337ea73
unknown <prathmesh.temkar@gmail.com>
2025-02-13 22:11:35 +0530

    edits and all changes

--- app/src/containers/valentine/page.tsx
+++ app/src/containers/valentine/page.tsx
@@ -10,12 +10,23 @@ const noTextsArr = [
   "Are you positive",
   "Just think about it",
   "if you say no, I'll be very sad",
-  "I'll be very sad",
-  "I'll be very very very sad",
+  "How about a kiss, please say YES",
   "Ok fine, I'll stop asking...",
   "Just kidding, PLEASE SAY YES",
-  "You're breaking my heart ;(",
   "No",
+];
+
+const imgArr = [
+  "/valentine/wedding.jpg",
+  "/valentine/cpl1.jpg",
+  "/valentine/cpl2.jpg",
+  "/valentine/teddy2.jpg",
+  "/valentine/goa1.jpg",
+  "/valentine/pose1.jpg",
+  "/valentine/kiss1.jpg",
+  "/valentine/trad1.jpg",
+  "/valentine/snap1.jpg",
+  "/valentine/teddy1.jpg",
 ];
 
 enum Stages {
@@ -43,19 +54,19 @@ export default function Valentine() {
 
   return (
     <section className="valentine-box select-none">
-      {stage === Stages.stageNo && (
+      {(stage === Stages.stageNo || stage === Stages.unhappy) && (
         <>
           <div className="text-center">
             <Image
               className="mx-auto"
-              src="/valentine/valentine-bear.gif"
+              src={imgArr[counter]}
               alt="valentine bear | valentine"
               width={250}
               height={250}
             />
           </div>
           <div className="text-2xl mt-2 text-gray-900 text-center">
-            Will you be my Valentine?
+            Will you be my Valentine Forever Binguss?
           </div>
           <div className="flex items-center justify-center mt-3 text-center">
             <button
@@ -63,31 +74,43 @@ export default function Valentine() {
               style={{ fontSize: counter ? `${(counter + 1) * 15}px` : "18px" }}
               onClick={() => setStage(Stages.stageYes)}
             >
-              Yes
+              {stage === Stages.unhappy
+                ? "You Actually dont have No as a choice"
+                : "Yes"}
             </button>
-            <button
-              className="bg-red-500 text-white rounded ms-2 py-1 px-2 text-[18px]"
-              onClick={changeNoText}
-            >
-              {noTextsArr[counter]}
-            </button>
+            {stage !== Stages.unhappy && (
+              <button
+                className="bg-red-500 text-white rounded ms-2 py-1 px-2 text-[18px]"
+                onClick={changeNoText}
+              >
+                {noTextsArr[counter]}
+              </button>
+            )}
           </div>
         </>
       )}
       {stage === Stages.stageYes && (
         <>
-          <Image
-            src="/valentine/bear-kiss-bear-kisses.gif"
-            alt="valentine kiss | valentine"
-            width={250}
-            height={250}
-          />
+          <div className="flex items-center justify-center gap-2">
+            <Image
+              src="/valentine/bear-kiss-bear-kisses.gif"
+              alt="valentine kiss | valentine"
+              width={250}
+              height={250}
+            />
+            <Image
+              src="/valentine/kiss3.jpg"
+              alt="valentine kiss"
+              width={250}
+              height={250}
+            />
+          </div>
           <div className="text-center text-2xl mt-2 text-gray-900">
-            {"Yay! Now I'm happy!"}
+            {"Yay! Lets Go for a walking date tomorrow"}
           </div>
         </>
       )}
-      {stage === Stages.unhappy && (
+      {/* {stage === Stages.unhappy && (
         <>
           <Image
             src="/valentine/unhappy-bear.gif"
@@ -99,7 +122,7 @@ export default function Valentine() {
             {"OK! Bye..."}
           </div>
         </>
-      )}
+      )} */}
     </section>
   );
 }
